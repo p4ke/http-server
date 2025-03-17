2adc5899917bdb267c4975ded8896f79f7736a69
tpa <144263473+p4ke@users.noreply.github.com>
2025-03-17 15:05:21 +0100

    Add comments to gradle buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    // Installiert die Gradle-Plugins, um hier eine Java-Applikation zu starten
     id("java-library")
     id("application")
 }
@@ -11,16 +12,23 @@ repositories {
 }
 
 dependencies {
+    // Mithilfe dieser Annotations-Bibliotheken können Laufzeitfehler vorgebeugt werden,
+    // da mit sogenannten "Nullabilitäts-Annotationen" markiert wird, ob eine
+    // Methode/Feld/Parameter/etc. einen "null"-Wert haben darf oder nicht;
+    // dies ist in Java nicht durch die Sprache selbst garantiert, sondern muss
+    // durch externe Bibliotheken gelöst werden
     compileOnlyApi("org.jspecify:jspecify:1.0.0")
     compileOnlyApi("org.jetbrains:annotations:26.0.2")
 }
 
 configure<JavaApplication> {
+    // Beschreibt den Klassennamen der Starter-Klasse
     mainClass = "$group.http.ServerMain"
 }
 
 configure<JavaPluginExtension> {
-    withSourcesJar()
+    // Stellt ein, welche "Java-Toolchain" von welchem Hersteller benötigt wird,
+    // damit die Ausführung dieses Projekts auch überall gleich läuft
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
         vendor = JvmVendorSpec.ADOPTIUM
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "HttpServer"
 
 plugins {
+    // Lädt automatisch die passende Java-Version für das Projekt herunter
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
